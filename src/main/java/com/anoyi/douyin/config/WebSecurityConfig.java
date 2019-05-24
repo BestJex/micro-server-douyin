0be4745579c539e93e68296e61292df8909a0887
anoy <545544032@qq.com>
2019-05-24 11:17:13 +0800

    修改配置

--- src/main/java/com/anoyi/douyin/config/WebSecurityConfig.java
+++ src/main/java/com/anoyi/douyin/config/WebSecurityConfig.java
@@ -9,8 +9,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().permitAll()
-                .anyRequest().permitAll();
+        http.authorizeRequests().anyRequest().permitAll().and().csrf().disable();
     }
 
 }
