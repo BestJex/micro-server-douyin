c3fe23a9b62a7fbc6baf1d06d5b46e5796f149bc
anoy <545544032@qq.com>
2019-05-24 11:02:47 +0800

    修改请求权限

--- src/main/java/com/anoyi/douyin/config/WebSecurityConfig.java
+++ src/main/java/com/anoyi/douyin/config/WebSecurityConfig.java
@@ -9,7 +9,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/").permitAll()
+        http.authorizeRequests().anyRequest().permitAll()
                 .anyRequest().permitAll();
     }
 
